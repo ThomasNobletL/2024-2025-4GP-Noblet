d53b10a9c199c91af13fcd8dca0ed742cbefb350
ThomasNobletL <thomas.nobletleroy@gmail.com>
2025-05-29 23:29:51 +0200

    Ajout docstring python

--- 04_PyApplication/Sensor.py
+++ 04_PyApplication/Sensor.py
@@ -8,6 +8,9 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout
 from PyQt6.QtCore import QThread, pyqtSignal, QTimer
 from PyQt6.uic import loadUi
 
+
+###Thread pour la connexion série et bluetooth###
+
 class SerialThread(QThread):
     data_received = pyqtSignal(int, int, int)  # émet 3 entiers reçus
 
@@ -18,6 +21,7 @@ class SerialThread(QThread):
         self._running = True
         self.ser =None
 
+    # Recupération des messages depuis le port
     def run(self):
         try:
             self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
@@ -44,7 +48,7 @@ class SerialThread(QThread):
         except serial.SerialException as e:
             print(f"Erreur de connexion série : {e}")
 
-
+    #Envoie des commandes pour controler la MCP41050
     def send_data(self, data: str):
         if self.ser and self.ser.is_open:
             self.ser.write(data.encode('utf-8'))
@@ -57,6 +61,7 @@ class SerialThread(QThread):
         self.wait()
 
 
+###Classe de l'interface###
 
 class MainInterface(QMainWindow):
     def __init__(self):
@@ -67,45 +72,45 @@ class MainInterface(QMainWindow):
             raise FileNotFoundError(f"Fichier UI introuvable : {ui_path}")
 
         loadUi(ui_path, self)
-
+        #Connexions aux boutons
         self.pushButton_Run.clicked.connect(self.toggle_recording)
         self.pushButton_Save.clicked.connect(self.save_data)
+        self._quit.clicked.connect(self.close)
+        self.send.clicked.connect(self.send_spinbox_value)
+        self.R2.setText(f"Ramp = 100000 \u03A9")
+        self.bluetooth_confirm.clicked.connect(self.confirm_selection)
 
+        #Initialisation du tableau de mesures
         self.recording = False
         self.data_storage = []
-        self.data_index = 0
+        self.index = 0
 
-        self._quit.clicked.connect(self.close)
-
+        #Initialisation du graphique
         self.graphWidget = pg.PlotWidget()
-        layout = QVBoxLayout(self.graph_container)  # graph_container est un QWidget dans ton UI
+        layout = QVBoxLayout(self.graph_container)
         layout.addWidget(self.graphWidget)
         self.graphWidget.setBackground('w')
         self.graphWidget.setTitle("Tension sortie (V)")
         self.graphWidget.setLabel('left', 'Voltage', units='V')
         self.graphWidget.setLabel('bottom', 'Points')
         self.graphWidget.showGrid(x=True, y=True)
-
-
-
-
         self.x = []
         self.y = []
-        self.index = 0
         self.data_line = self.graphWidget.plot(self.x, self.y, pen=pg.mkPen(color='b', width=2))
 
+        #Recherches des ports disponibles
         self.populate_ports()
 
-        self.send.clicked.connect(self.send_spinbox_value)
-        self.R2.setText(f"Ramp = 100000 \u03A9")
-        self.bluetooth_confirm.clicked.connect(self.confirm_selection)
+        #Par défaut, aucun thread actif
         self.serial_thread = None
-        
+
+    # Fonction de ermeture propre du programme
     def closeEvent(self, event):
         if self.serial_thread is not None:
             self.serial_thread.stop()
         event.accept()
 
+    # Fonction de recherche des ports disponibles
     def populate_ports(self):
         ports = serial.tools.list_ports.comports()
         self.bluetooth_box.clear()
@@ -113,7 +118,7 @@ class MainInterface(QMainWindow):
             display_text = f"{port.device}: {port.description} [{port.hwid}]"
             self.bluetooth_box.addItem(display_text, port.device)
 
-    
+    # Fonction de connexion avec le port choisi
     def confirm_selection(self):
         selected_port = self.bluetooth_box.currentData()
         print(f"Port sélectionné : {selected_port}")
@@ -123,11 +128,13 @@ class MainInterface(QMainWindow):
 
         if self.serial_thread is not None:
                 self.serial_thread.stop()
-    
+                self.serial_thread = None
+        # Test de la connexion sur le port avant de creer le thread
         try:
-            # Tester l'ouverture avant de créer le thread
+
             test_serial = serial.Serial(selected_port, baudrate=9600, timeout=1)
             test_serial.close()
+
             self.serial_conn = serial.Serial(selected_port, baudrate=9600, timeout=1)
 
             self.serial_thread = SerialThread(selected_port,baudrate=9600)
@@ -138,21 +145,26 @@ class MainInterface(QMainWindow):
         except serial.SerialException as e:
             print(f"Erreur de connexion : {e}")
             self.value.setText("Connexion failed")
-        
+
+    # Mises à jour des valeurs dans l'interface, le tableau et le graphique
     def update_values(self, a, b):
         sortie = a*5.0/1023.0 #conversion en volt
         gain = a / b if b != 0 else 0 #calcul gain observé, inutile ici
         self.value.setText(f"Mes = {sortie} V")
         self.gain.setText(f"Gain = {gain}")
+
         if self.recording:
-            self.x.append(self.index)
+            self.data_storage.append((self.index, sortie)) #Stockage pour le txt
+
+            self.x.append(self.index) #Update du graphique
             self.y.append(sortie)
             self.data_line.setData(self.x, self.y)
             self.index += 1
 
+    # Fonction d'envoie des valeurs de résistances au MCP41050
     def send_spinbox_value(self):
         value = self.spinBox.value()
-        MCP_res = 100000/(value -1)
+        MCP_res = 100000/value
         D = round(MCP_res-125*(256/50000))
 
         self.R1.setText(f"RMCP = {MCP_res:.1f} \u03A9")
@@ -160,13 +172,15 @@ class MainInterface(QMainWindow):
         if isinstance(self.serial_thread, SerialThread):
             self.serial_thread.send_data(f"SET{D}\n")
         else:
-            print("Aucune connexion série active pour l'envoi.")
+            print("Aucune connexion liaison série active pour l'envoi.")
+
+    #  Fonction de sauvegarde du tableau récupéré
 
     def save_data(self):
         if not self.data_storage:
             print("Aucune donnée à sauvegarder.")
             return
-        filename = "data_sortie.txt"
+        filename = "FlexOutMes.txt"
         try:
             with open(filename, "w") as f:
                 f.write("Index\tSortie(V)\n")
@@ -176,15 +190,20 @@ class MainInterface(QMainWindow):
         except Exception as e:
             print(f"Erreur lors de la sauvegarde : {e}")
 
+    # Fonction de début de stockage dans la tableau
+
     def toggle_recording(self):
         self.recording = not self.recording
         if self.recording:
-            self.pushButton_Run.setText("Arrêter")
+            self.pushButton_Run.setText("STOP")
             self.data_storage.clear()
-            self.data_index = 0
+            self.index = 0              #Reinitialisation des mesures precedentes
+            self.x.clear()
+            self.y.clear()
+            self.data_line.setData([], [])
         else:
-            self.pushButton_Run.setText("Démarrer")
-        print(f"Enregistrement {'démarré' if self.recording else 'arrêté'}")
+            self.pushButton_Run.setText("RUN")
+        print(f"Enregistrement {'RUN' if self.recording else 'STOP'}")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
